a663230b2330786d414a6214adbbfc4a9ad9c2aa
trollgugui13 <84552703+trollgugui13@users.noreply.github.com>
2022-06-23 09:31:06 -0300

    Add files via upload

--- chamados/frontend/estatisticas.php
+++ chamados/frontend/estatisticas.php
@@ -5,8 +5,7 @@
   <script src="https://cdn.jsdelivr.net/npm/chart.js@3.8.0/dist/chart.min.js" integrity="sha256-cHVO4dqZfamRhWD7s4iXyaXWVK10odD+qp4xidFzqTI=" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  <link href="styleadmin.css" rel="stylesheet" type="text/css" />
-  <script src="estatisticas.js" type="text/javascript"></script>
+  <link href="style.css" rel="stylesheet" type="text/css" />
 
 </head>
 
